366bad61dbb986b64d8ddbf888599a0f4b4064a9
nahushg <nahushg@vt.edu>
2017-03-13 15:16:24 -0400

    Adding 3D sigma dictionary

--- scripts/global_var.py
+++ scripts/global_var.py
@@ -5,5 +5,6 @@ initialTrainingEpisodes = 10
 global delta_t
 global sigmaDictX
 global sigmaDictY
+global sigmaDictZ
 current_state_for_grid_world_reference = (-GRID,-GRID)
 envList = ['grid','gazebo']
